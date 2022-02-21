0643c973d02d91352459534ad6a7dd786229e77e
Mahim <mahim216@gmail.com>
2022-02-21 15:41:23 -0500

    removed unused imports

--- src/main/java/frc/robot/commands/TestCommandGroup.java
+++ src/main/java/frc/robot/commands/TestCommandGroup.java
@@ -1,15 +1,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Shooter;
 
 public class TestCommandGroup extends SequentialCommandGroup {
-    public TestCommandGroup(DriveTrain dt, Shooter shooter) {
-        addCommands(
-            new DriveTimedCommand(dt, 5),
-            new AutoShoot(shooter)
-        );
-    }
+	public TestCommandGroup(DriveTrain dt, Shooter shooter) {
+		addCommands(new DriveTimedCommand(dt, 5), new AutoShoot(shooter));
+	}
 }

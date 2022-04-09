ed0d2942c08b4a1f5619b8538b1fdd57bf961214
IronRam8241 <60673902+IronRam8241@users.noreply.github.com>
2022-04-09 08:29:36 -0400

    making auton go backwaards

--- src/main/java/frc/robot/commands/auton/DriveTimedCommand.java
+++ src/main/java/frc/robot/commands/auton/DriveTimedCommand.java
@@ -32,7 +32,7 @@ public class DriveTimedCommand extends CommandBase {
 	// Called every time the scheduler runs while the command is scheduled.
 	@Override
 	public void execute() {
-		drivetrain.drive(0.5, 0.5);
+		drivetrain.drive(-0.5, -0.5);
 	}
 
 	// Called once the command ends or is interrupted.

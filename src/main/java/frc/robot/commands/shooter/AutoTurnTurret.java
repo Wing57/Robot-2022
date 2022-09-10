514554275973a23845f00fbade18f613192020c2
WIng57 <ddariusd007@gmail.com>
2022-09-10 18:48:46 -0400

    implementing mahims array lists because for some reason they werent put into effect

--- src/main/java/frc/robot/commands/shooter/AutoTurnTurret.java
+++ src/main/java/frc/robot/commands/shooter/AutoTurnTurret.java
@@ -10,42 +10,42 @@ import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Vision;
 
 public class AutoTurnTurret extends CommandBase {
-  /** Creates a new turret. */
-  private Vision vision;
-  private Shooter shooter;
+	/** Creates a new turret. */
+	private Vision vision;
+  
 
-  public AutoTurnTurret(Vision vision, Shooter shooter) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.vision = vision;
-    this.shooter = shooter;
-  }
+	public AutoTurnTurret(Vision vision, Shooter shooter) {
+		// Use addRequirements() here to declare subsystem dependencies.
+		this.vision = vision;
+    
+	}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+	}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (vision.getHorizontalOffset() < -0.5) {
-      // shooter.setTurretSpeed(-0.2);
-    } else if (vision.getHorizontalOffset() > 0.5) {
-      // shooter.setTurretSpeed(0.2);
-    } else {
-      // shooter.setTurretSpeed(0);
-    }
-  }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		if (vision.getHorizontalOffset() < -0.5) {
+			// shooter.setTurretSpeed(-0.2);
+		} else if (vision.getHorizontalOffset() > 0.5) {
+			// shooter.setTurretSpeed(0.2);
+		} else {
+			// shooter.setTurretSpeed(0);
+		}
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    // shooter.setTurretSpeed(0);
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		// shooter.setTurretSpeed(0);
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }

123b33eb9630bddee9407332b76dc367017356c1
IronRam8241 <60673902+IronRam8241@users.noreply.github.com>
2022-09-27 11:44:18 -0400

    idk choom something abt the lisence

--- src/main/java/frc/robot/commands/intake/Intakeball.java
+++ src/main/java/frc/robot/commands/intake/Intakeball.java
@@ -1,6 +1,7 @@
 // Copyright (c) Darius and other Darius contributors.
-// Open Source Software; you can modify and/or share it under the terms of 
-// the WPILib BSD license file and DARI lisence in the root directory of this project.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file and DARI lisence in the root directory of this
+// project.
 
 package frc.robot.commands.intake;
 
@@ -10,28 +11,28 @@ import frc.robot.Constants;
 import frc.robot.subsystems.Intake;
 
 public class Intakeball extends CommandBase {
-	Intake intake;
+  Intake intake;
 
-	public Intakeball(Intake intake) {
-		this.intake = intake;
-		addRequirements(intake);
-	}
+  public Intakeball(Intake intake) {
+    this.intake = intake;
+    addRequirements(intake);
+  }
 
-	// Called every time the scheduler runs while the command is scheduled.
-	@Override
-	public void execute() {
-		intake.setIntakeMotor(Constants.INTAKE_SPEED);
-	}
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intake.setIntakeMotor(Constants.INTAKE_SPEED);
+  }
 
-	// Called once the command ends or is interrupted.
-	@Override
-	public void end(boolean interrupted) {
-		intake.stop();
-	}
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    intake.stop();
+  }
 
-	// Returns true when the command should end.
-	@Override
-	public boolean isFinished() {
-		return false;
-	}
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,6 +1,7 @@
 // Copyright (c) Darius and other Darius contributors.
-// Open Source Software; you can modify and/or share it under the terms of 
-// the WPILib BSD license file and DARI lisence in the root directory of this project.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file and DARI lisence in the root directory of this
+// project.
 
 package frc.robot.subsystems;
 

14c14817c12b1f8a066c898a1b7add450b3ee584
WIng57 <ddariusd007@gmail.com>
2022-10-02 21:13:40 -0400

    reverting vision back to APR 8 2022

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,76 +4,72 @@
 
 package frc.robot.subsystems;
 
-
+import com.rambots4571.rampage.vision.Limelight;
 import com.rambots4571.rampage.vision.Limelight.CamMode;
 import com.rambots4571.rampage.vision.Limelight.LedMode;
+import com.rambots4571.rampage.vision.ReadValue;
 
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.Shooters;
 
 public class Vision extends SubsystemBase {
-  public static NetworkTable table = NetworkTableInstance.getDefault().getTable("Limelight");
-  
+	private final Limelight limelight = Limelight.getInstance();
 
-  /** Creates a new Limelight. */
-  public Vision() {
-  }
+	/** Creates a new Limelight. */
+	public Vision() {
+	}
 
-  // Gets the number of valid targets (0 or 1)
-  public static boolean hasValidTarget() {
-    return table.getEntry("tv").getNumber(0.0).intValue() == 1;
-  }
+	// Gets the number of valid targets (0 or 1)
+	public boolean hasValidTarget() {
+		return limelight.getValue(ReadValue.tv) == 1;
+	}
 
-  // Gets the horizontal offset of the center crosshair from the target
-  public static double getHorizontalOffset() {
-    return table.getEntry("tx").getDouble(Integer.MAX_VALUE);
-  }
+	// Gets the horizontal offset of the center crosshair from the target
+	public double getHorizontalOffset() {
+		return limelight.getValue(ReadValue.tx);
+	}
 
-  // Gets the vertical offset of the center crosshair from the target
-  public static double getVerticalOffset() {
-    return table.getEntry("ty").getDouble(Integer.MAX_VALUE);
-  }
+	// Gets the vertical offset of the center crosshair from the target
+	public double getVerticalOffset() {
+		return limelight.getValue(ReadValue.ty);
+	}
 
-  // Gets the estimated distance from the robot to the target
-  public double getEstimatedDistance() {
-    double offset = getVerticalOffset();
-    // return 0.0;
-    return Shooters.LIMELIGHT_DISTANCE_K[0] * Math.pow(offset, 2)
-      + Shooters.LIMELIGHT_DISTANCE_K[1] * offset + Shooters.LIMELIGHT_DISTANCE_K[2];
-  }
+	// Gets the estimated distance from the robot to the target
+	public double getEstimatedDistance() {
+		double offset = getVerticalOffset();
+		// return 0.0;
+		return Shooters.LIMELIGHT_DISTANCE_K[0] * Math.pow(offset, 2)
+		  + Shooters.LIMELIGHT_DISTANCE_K[1] * offset + Shooters.LIMELIGHT_DISTANCE_K[2];
+	}
 
-  // Gets the active pipeline (0 to 9)
-  public int getPipe() {
-    return table.getEntry("rambotspipeline").getNumber(-1).intValue();
-  }
+	// Gets the active pipeline (0 to 9)
+	public int getPipe() {
+		return limelight.getEntry("rambotspipeline").getNumber(-1).intValue();
+	}
 
-  // Sets the active pipeline (0 to 9)
-  public void setPipe(int pipeNum) {
-    table.getEntry("pipeline").setNumber(pipeNum);
-    
-  }
+	// Sets the active pipeline (0 to 9)
+	public void setPipe(int pipeNum) {
+		limelight.setPipeline(pipeNum);
+		;
+	}
 
-  // Sets the LED state. LEDs on = 3, LEDs off = 1
-  public void setLEDMode(LedMode mode) {
-    table.getEntry("ledmode").setNumber(3);
-  }
+	// Sets the LED state. LEDs on = 3, LEDs off = 1
+	public void setLEDMode(LedMode mode) {
+		limelight.setLedMode(mode);
+	}
 
-  // Set's the limelight's state of operation. Visiom Processing = 0, Driver
-  // Camera = 1
-  public void setCameraMode(CamMode mode) {
-    table.getEntry("camMode").setNumber(0);
-  }
+	// Set's the limelight's state of operation. Visiom Processing = 0, Driver
+	// Camera = 1
+	public void setCameraMode(CamMode mode) {
+		limelight.setCamMode(mode);
+	}
 
-  
-  public static void upadateTelemetry() {
-    SmartDashboard.putBoolean("Limelight Valid Target", hasValidTarget());
-    SmartDashboard.putNumber("xOff", getHorizontalOffset());
-    SmartDashboard.putNumber("yOff", getVerticalOffset());
-  }
+	@Override
+	public void initSendable(SendableBuilder builder) {
+		builder.setSmartDashboardType("Limelight Values");
+		builder.addBooleanProperty("has valid target", this::hasValidTarget, null);
+		builder.addDoubleProperty("xOff", this::getHorizontalOffset, null);
+		builder.addDoubleProperty("yOff", this::getVerticalOffset, null);
+	}
 }

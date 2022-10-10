19e16cc4e040b377b90f3c1f38e28937371829fe
WIng57 <ddariusd007@gmail.com>
2022-10-10 18:52:02 -0400

    Creating SetShooterSpeed instancommand

---
+++ src/main/java/frc/robot/commands/shooter/SetShooterSpeed.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Shooter;
+
+public class SetShooterSpeed extends InstantCommand {
+
+  private final Shooter shooter;
+  private final double targetSpeed;
+  private final double targetBackSpeed;
+
+  public SetShooterSpeed(Shooter s, double targetSpeed, double targetBackSpeed) {
+    shooter = s;
+    this.targetSpeed = targetSpeed;
+    this.targetBackSpeed = targetBackSpeed;
+    addRequirements(shooter);
+  }
+
+  @Override
+  public void initialize() {
+    shooter.setShooterSpeed(targetSpeed);
+    shooter.setBackSpinSpeed(targetBackSpeed);
+  }
+}

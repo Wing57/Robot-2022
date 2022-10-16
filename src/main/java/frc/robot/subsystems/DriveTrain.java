dd0935a6b1b6559f72135f68bf77dab81c1ed1ba
WIng57 <ddariusd007@gmail.com>
2022-10-16 01:49:49 -0400

    pushing field to smartdashboard

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -25,6 +25,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import frc.robot.Constants;
@@ -281,6 +282,8 @@ public class DriveTrain extends SubsystemBase {
   public void periodic() {
     updateOdometry();
     field.setRobotPose(getPose());
+
+    SmartDashboard.putData("Field", field);
   }
 
   @Override

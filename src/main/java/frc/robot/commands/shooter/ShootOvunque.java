8a6dc44d806fe6c9c642853db2162b83cf5ce04f
Wing57 <ddariusd007@gmail.comm>
2022-10-18 12:23:00 -0400

    theoritcally finishing shootanywhere command

--- src/main/java/frc/robot/commands/shooter/ShootOvunque.java
+++ src/main/java/frc/robot/commands/shooter/ShootOvunque.java
@@ -4,20 +4,40 @@
 
 package frc.robot.commands.shooter;
 
+import com.stuypulse.stuylib.streams.booleans.BStream;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Vision;
+import frc.robot.Constants.Shooters;
 import frc.robot.subsystems.Shooter;
 
 public class ShootOvunque extends CommandBase {
 
-  Shooter shooter;
+  Shooter m_shooter;
+  Vision m_vision;
 
-  public ShootOvunque(Shooter s) {
-    shooter = s;
+  BStream shooterReady;
+
+  public ShootOvunque(Shooter shooter, Vision vision) {
+    m_shooter = shooter;
+    m_vision = vision;
+
+    shooterReady = BStream.create(() -> shooter.isReady())
+    .and(() -> vision.hasValidTarget());
+
     addRequirements(shooter);
   }
 
   @Override
   public void execute() {
-    
+    if (m_vision.hasValidTarget()) {
+      
+      double distance = m_vision.getEstimatedDistance();
+      m_shooter.setShooterVelocity(Shooters.SHOOTER_SPEED_INTERPOLATOR.getInterpolatedValue(distance));
+      m_shooter.setBackSpinVelocity(Shooters.BACKSPIN_SPEED_INTERPOLATOR.getInterpolatedValue(distance));
+    }
+      else {
+        System.out.println("No Target Innit");
+    }
   }
 }

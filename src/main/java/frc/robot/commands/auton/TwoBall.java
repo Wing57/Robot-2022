243e5ce43d8be04b23c4d8e9b1ab30b6592d96e5
WIng57 <ddariusd007@gmail.com>
2022-10-22 00:00:20 -0400

    Re-arranging auton routine

--- src/main/java/frc/robot/commands/auton/TwoBall.java
+++ src/main/java/frc/robot/commands/auton/TwoBall.java
@@ -8,20 +8,23 @@ import frc.robot.RobotContainer;
 import frc.robot.commands.index.IndexBall;
 import frc.robot.commands.intake.IntakeBallForever;
 import frc.robot.commands.intake.intakeExtend;
+import frc.robot.commands.shooter.InsideShot;
 import frc.robot.commands.shooter.TarmacShot;
 
 public class TwoBall extends SequentialCommandGroup {
 
   public TwoBall(RobotContainer container) {
     addCommands(
-        new TarmacShot(container.shooter),
+        new InsideShot(container.shooter),
         new intakeExtend(container.intake),
         new WaitCommand(AutoConstants.INTAKE_EXTEND),
         new IntakeBallForever(container.intake),
-        new WaitCommand(AutoConstants.SHOOTER_INITIALIZE),
-        new IndexBall(container.index));
+        new IndexBall(container.index),
+        new WaitCommand(AutoConstants.SHOOTER_INITIALIZE));
 
-    addCommands(new DriveTimedCommand(container.driveTrain, 3));
+    addCommands(
+      new InsideShot(container.shooter),
+      new DriveTimedCommand(container.driveTrain, 3));
   }
 
   @Override
